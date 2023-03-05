0a511f18687c8c6fcb4f57fc289eaff18427f9eb
erion-meta <metaerion79@gmail.com>
2023-03-05 23:08:04 +0100

    conversionAndCoercion

---
+++ Fundamentals Part 1/5_conversionAndCoercion.js
@@ -0,0 +1,11 @@
+let test1 = "9" - "5";
+let test2 = "19" - "13" + "17";
+let test3 = "19" - "13" + 17;
+let test4 = "123" < 57;
+let test5 = 5 + 6 + "4" + 9 - 4 - 2;
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
