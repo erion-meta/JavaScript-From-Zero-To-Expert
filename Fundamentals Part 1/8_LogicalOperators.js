d8cf57cccba4db5fed874f7a1da959543bf5e4d6
erion-meta <metaerion79@gmail.com>
2023-03-05 23:23:01 +0100

    logical operators

---
+++ Fundamentals Part 1/8_LogicalOperators.js
@@ -0,0 +1,10 @@
+let myCountry = "Albania";
+let population = 2;
+let isIsland = false;
+let language = "albanian";
+
+if (language === "english" && population < 50 && !isIsland) {
+  console.log("Perfect country to live");
+} else {
+  console.log("Lets find another country");
+}
