0eb6c226645dc99d6b98a25705c0d6dd12bd2806
erion-meta <metaerion79@gmail.com>
2023-03-05 23:34:11 +0100

    switch statements

---
+++ Fundamentals Part 1/9_SwitchStatements.js
@@ -0,0 +1,27 @@
+const language = "albania";
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4'");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  case "albania":
+    console.log(
+      "Jaaaaaaaaaaaaaaaaaaammmm shqiiiiiiiiippttttaaaaaar ooooooooo mmmiiiiiggooooooo"
+    );
+    break;
+  default:
+    console.log("Great language too :D");
+}
