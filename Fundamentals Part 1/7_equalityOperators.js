aadb6557b12d31af28b5adf68aec04bf2db6fc5d
erion-meta <metaerion79@gmail.com>
2023-03-05 23:15:33 +0100

    equality op

---
+++ Fundamentals Part 1/7_equalityOperators.js
@@ -0,0 +1,9 @@
+let numNeighbours = 1;
+
+if (numNeighbours === 1) {
+  console.log("There is only 1 border");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
