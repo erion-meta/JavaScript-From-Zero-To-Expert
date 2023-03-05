6cffae5f756fc3e60bf65ba998b5fa1a6f9379b8
erion-meta <metaerion79@gmail.com>
2023-03-05 23:00:38 +0100

    if else

---
+++ Fundamentals Part 1/5ifElse.js
@@ -0,0 +1,12 @@
+let countryAlb = "Albania";
+let countryIt = "Italy";
+let albaniaPopulation = 2832439;
+let italyPopulation = 60225131;
+
+if (albaniaPopulation > italyPopulation) {
+  let message = `${countryAlb} has a higher pupolation of ${albaniaPopulation} million than ${countryIt} that has ${italyPopulation}`;
+  console.log(message);
+} else {
+  let message = `${countryIt} has a higher pupolation of ${italyPopulation} million than ${countryAlb} that has ${albaniaPopulation}`;
+  console.log(message);
+}
