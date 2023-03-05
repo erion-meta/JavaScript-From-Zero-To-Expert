c192161b8d6cf8c08481478a07d49f6148dfdc2e
erion-meta <metaerion79@gmail.com>
2023-03-05 23:53:44 +0100

    challenges 4 fundamental part 1 done

--- Fundamentals Part 1/challenges4.js
+++ Fundamentals Part 1/challenges4.js
@@ -1,4 +1,13 @@
-let billValue = 50;
-let tip;
+let billValue = 430;
+let tip = 0;
+let billToPay = 0;
 
-billValue <= 50 && billValue >= 300 ? 0.15 : 0.2;
+billValue >= 50 && billValue <= 300 ? (tip = "15%") : (tip = "20%");
+
+tip === "15%"
+  ? (billToPay = billValue + billValue * 0.15)
+  : (billToPay = billValue + billValue * 0.2);
+
+console.log(`Bill value = ${billValue}`);
+console.log(`Tip = ${tip}`);
+console.log(`Final bill to pay = ${billToPay}`);
