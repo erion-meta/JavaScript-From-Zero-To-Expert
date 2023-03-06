4044acbb29def6becb8398c27148d02867c47b8d
erion-meta <metaerion79@gmail.com>
2023-03-06 22:46:02 +0100

    function declaration

--- Fundamentals Part 2/2_FunctionDeclarations.js
+++ Fundamentals Part 2/2_FunctionDeclarations.js
@@ -1,1 +1,11 @@
-console.log("Hello function");
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const country1 = percentageOfWorld1(1441);
+const country2 = percentageOfWorld1(60);
+const country3 = percentageOfWorld1(150);
+
+console.log(`China population percentage is ${country1}`);
+console.log(`Italy population percentage is ${country2}`);
+console.log(`Brazil population percentage is ${country3}`);
