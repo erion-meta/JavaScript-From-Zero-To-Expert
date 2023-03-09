b9428eccc2276ce2898ea572c1938a314d04d03f
erion-meta <metaerion79@gmail.com>
2023-03-09 23:31:15 +0100

    looping array assingment

---
+++ Fundamentals Part 2/11_LoopingArrays.js
@@ -0,0 +1,11 @@
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const populations = [10, 1441, 332, 83];
+const percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+  const perc = percentageOfWorld1(populations[i]);
+  percentages2.push(perc);
+}
+console.log(percentages2);
