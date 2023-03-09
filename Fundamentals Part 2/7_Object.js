121af9901b647cc2ed85b4a39df32677dc474dd2
erion-meta <metaerion79@gmail.com>
2023-03-09 22:44:32 +0100

    object assingment

---
+++ Fundamentals Part 2/7_Object.js
@@ -0,0 +1,9 @@
+const myCountry = {
+  country: "Albania",
+  capital: "Tirana",
+  language: "Shqip",
+  population: "2.8 million",
+  neighbours: ["Montenegro", "Kosova", "North Macedonia", "Greece"],
+};
+
+console.log(myCountry);
