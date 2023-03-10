8efa964278329de5a49f46a168cff1066ba9fc3b
erion-meta <metaerion79@gmail.com>
2023-03-10 23:55:32 +0100

    handling click events

--- 3_Dom Events Fundamentals/Guess My Number/script.js
+++ 3_Dom Events Fundamentals/Guess My Number/script.js
@@ -1,5 +1,17 @@
-document.querySelector(".message").textContent = "Correct Number!";
+//
 
-document.querySelector(".number").textContent = 20;
-document.querySelector(".score").textContent = 13;
-document.querySelector(".highscore").textContent = 17;
+// document.querySelector(".number").textContent = 20;
+// document.querySelector(".score").textContent = 13;
+// document.querySelector(".highscore").textContent = 17;
+
+const messageText = document.querySelector(".message");
+const checkButton = document.querySelector(".check");
+
+checkButton.addEventListener("click", function () {
+  const guesInput = Number(document.querySelector(".guess").value);
+  console.log(guesInput);
+
+  if (!guesInput) {
+    messageText.textContent = "No number";
+  }
+});
