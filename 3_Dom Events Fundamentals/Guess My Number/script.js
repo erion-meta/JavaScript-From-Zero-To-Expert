496ff48b2b3915d89b689cab628e4ce90e172cf6
erion-meta <metaerion79@gmail.com>
2023-03-10 23:30:36 +0100

    manipulating elements, script.js

--- 3_Dom Events Fundamentals/Guess My Number/script.js
+++ 3_Dom Events Fundamentals/Guess My Number/script.js
@@ -1,1 +1,5 @@
+document.querySelector(".message").textContent = "Correct Number!";
 
+document.querySelector(".number").textContent = 20;
+document.querySelector(".score").textContent = 13;
+document.querySelector(".highscore").textContent = 17;
