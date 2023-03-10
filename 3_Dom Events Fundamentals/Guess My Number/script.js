2d1b1adaca5811aed178f8452cc34f21585f484f
erion-meta <metaerion79@gmail.com>
2023-03-11 00:00:58 +0100

    error fixing..

--- 3_Dom Events Fundamentals/Guess My Number/script.js
+++ 3_Dom Events Fundamentals/Guess My Number/script.js
@@ -1,9 +1,3 @@
-//
-
-// document.querySelector(".number").textContent = 20;
-// document.querySelector(".score").textContent = 13;
-// document.querySelector(".highscore").textContent = 17;
-
 const messageText = document.querySelector(".message");
 const checkButton = document.querySelector(".check");
 
