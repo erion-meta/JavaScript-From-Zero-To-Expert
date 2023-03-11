0c07f218869c27beedd47c630e7cae4903f43fba
erion-meta <metaerion79@gmail.com>
2023-03-12 00:15:26 +0100

    high score element

--- 3_Dom Events Fundamentals/Guess My Number/script1.js
+++ 3_Dom Events Fundamentals/Guess My Number/script1.js
@@ -1,15 +1,19 @@
-const messageText = document.querySelector(".message");
-const checkButton = document.querySelector(".check");
-let randomNumber = Math.floor(Math.random() * 21);
+const messageText = document.querySelector(".message"); // mesage text variable
+const checkButton = document.querySelector(".check"); // button to check number
+let randomNumber = Math.floor(Math.random() * 21); // random number
+
 // score variables
 let score = document.querySelector(".score");
 let scoreValue = Number(score.innerHTML);
+
+// highscore
+let highscore = document.querySelector(".highscore");
 
 console.log(scoreValue);
 console.log(randomNumber);
 
 checkButton.addEventListener("click", function () {
-  const guesInput = Number(document.querySelector(".guess").value);
+  const guesInput = Number(document.querySelector(".guess").value); // Number to guess by the user
 
   console.log(guesInput);
 
@@ -17,13 +21,16 @@ checkButton.addEventListener("click", function () {
     messageText.textContent = "No number";
   } else if (guesInput === randomNumber) {
     messageText.textContent = "Correct Number";
+    highscore.innerHTML = scoreValue.toString();
   } else if (guesInput > randomNumber) {
     messageText.textContent = "To high";
     scoreValue--;
     score.innerHTML = scoreValue.toString();
+    highscore.innerHTML = scoreValue.toString();
   } else {
     messageText.textContent = "To low";
     scoreValue--;
     score.innerHTML = scoreValue.toString();
+    highscore.innerHTML = scoreValue.toString();
   }
 });
