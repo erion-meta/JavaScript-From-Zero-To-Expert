7401138b534a6cce2ad6c9bea7bd3159b043a9e6
erion-meta <metaerion79@gmail.com>
2023-03-15 23:43:09 +0100

    scoping..

--- 4_Behind The Scenes/script.js
+++ 4_Behind The Scenes/script.js
@@ -1,6 +1,16 @@
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    const output = `${firstName} is ${age} years old, and born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 2000 && birthYear <= 2010) {
+      const output2 = `Ohoo, ${firstName} you are a Trrim`;
+      console.log(output2);
+    }
+  }
+  printAge();
   return age;
 }
 
