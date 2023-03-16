2dc5dddd402ab1e60b533f67ffbd45f1e0bd8843
erion-meta <metaerion79@gmail.com>
2023-03-16 20:30:55 +0100

    hoisting functions

--- 4_Behind The Scenes/script.js
+++ 4_Behind The Scenes/script.js
@@ -19,12 +19,28 @@
 // const firstName = "Erion";
 // calcAge(2001);
 
-// HOISTING
+// HOISTING variables
 
-console.log(job); // undefined
-console.log(year); // Cannot access 'year' before initialization
-console.log(price); // Cannot access 'price' before initialization
+// console.log(job); // undefined
+// console.log(year); // Cannot access 'year' before initialization
+// console.log(price); // Cannot access 'price' before initialization
 
-var job = "Lawyer";
-let year = 2023;
-const price = 2000;
+// var job = "Lawyer";
+// let year = 2023;
+// const price = 2000;
+
+// HOISTINg functions
+
+// console.log(addNum1(2, 3)); // 5
+// console.log(addNum2(2, 3)); // Cannot access 'addNum2' before initialization
+// console.log(addNum3(2, 3)); // Cannot access 'addNum3' before initialization
+
+// function addNum1(a, b) {
+//   return a + b;
+// }
+
+// const addNum2 = function (a, b) {
+//   return a + b;
+// };
+
+// const addNum3 = (a, b) => a + b;
