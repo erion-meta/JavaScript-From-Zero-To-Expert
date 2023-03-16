9b63f82f594cb788bc765fae025e5068e39757ef
erion-meta <metaerion79@gmail.com>
2023-03-16 20:22:58 +0100

    hoisting, js behind the scenes

--- 4_Behind The Scenes/script.js
+++ 4_Behind The Scenes/script.js
@@ -1,18 +1,30 @@
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
+// SCOPING
 
-  function printAge() {
-    const output = `${firstName} is ${age} years old, and born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
 
-    if (birthYear >= 2000 && birthYear <= 2010) {
-      const output2 = `Ohoo, ${firstName} you are a Trrim`;
-      console.log(output2);
-    }
-  }
-  printAge();
-  return age;
-}
+//   function printAge() {
+//     const output = `${firstName} is ${age} years old, and born in ${birthYear}`;
+//     console.log(output);
 
-const firstName = "Erion";
-calcAge(2001);
+//     if (birthYear >= 2000 && birthYear <= 2010) {
+//       const output2 = `Ohoo, ${firstName} you are a Trrim`;
+//       console.log(output2);
+//     }
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = "Erion";
+// calcAge(2001);
+
+// HOISTING
+
+console.log(job); // undefined
+console.log(year); // Cannot access 'year' before initialization
+console.log(price); // Cannot access 'price' before initialization
+
+var job = "Lawyer";
+let year = 2023;
+const price = 2000;
