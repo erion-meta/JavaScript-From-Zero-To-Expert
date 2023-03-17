6b1729c099ffcf208a47f65ec3c199d059856933
erion-meta <metaerion79@gmail.com>
2023-03-17 23:23:24 +0100

    Nullish operator

---
+++ 5_Data Structures-Operators/6_NullishCoalescingOP.js
@@ -0,0 +1,58 @@
+const restaurant = {
+  name: "Ceren Ismet Shehu",
+  location: "Surrel, Tirana(Albania)",
+  categories: ["Albanian", "Meat", "Traditional", "Organic"],
+  starterMenu: [
+    "Fergese",
+    "Trahana",
+    "Salce Kosi",
+    "Pispili",
+    "Sallate Jeshile",
+  ],
+  mainMenu: [
+    "Mish Qingji",
+    "Berxolle Vici",
+    "Mish i Thate",
+    "Jufka",
+    "Tave Kosi ",
+  ],
+  openingHours: {
+    thu: {
+      open: 11,
+      close: 24,
+    },
+    fri: {
+      open: 13,
+      close: 24,
+    },
+    sat: {
+      open: 11,
+      close: 24,
+    },
+    sun: {
+      open: 11,
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at time ${time}`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
+
+restaurant.numGuest = 0;
+const guests = restaurant.numGuest || 10;
+console.log(guests);
+
+// Nullis: null and undefined
+const guestCorrect = restaurant.numGuest ?? 10;
+console.log(guestCorrect);
