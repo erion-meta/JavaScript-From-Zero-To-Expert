4f420fb0f1e652159613df672bfeb7b08cc7dcc8
erion-meta <metaerion79@gmail.com>
2023-03-18 23:41:08 +0100

    Challenges 4 | Fundamentals P2, Done!

--- 2_Fundamentals Part 2/challenges4.js
+++ 2_Fundamentals Part 2/challenges4.js
@@ -13,3 +13,15 @@ for (let i = 0; i < bills.length; i++) {
   totals.push(tip + bills[i]);
 }
 console.log("Bills: ", bills, "Tips: ", tips, "Totals", totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
