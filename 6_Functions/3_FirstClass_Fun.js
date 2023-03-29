78bb6138e16230585636df8cbe1f8df2b936dfb2
erion-meta <metaerion79@gmail.com>
2023-03-29 17:25:31 +0200

    First Class/ High Order Functions

---
+++ 6_Functions/3_FirstClass_Fun.js
@@ -0,0 +1,19 @@
+// First Class and High Order Functions
+
+/* 1. First Class Functions:
+- JavaScript treats funcrions as first class citizens
+- This means that functions are simply values
+- Functions are just another "type" of object
+
+ex: const add = function(a,b){
+    return a+b;
+}
+ */
+
+/* 2. High Order Functions
+- A function that recieves another function as an argument,
+ that returns a new functions, or both
+- This is only because of first class functions 
+
+ex: btn.addEventListener("click", greet)
+*/
